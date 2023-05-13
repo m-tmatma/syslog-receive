7e881177f770dc76812abea3e30f69e177d4812e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-13 16:31:23 +0900

    comment out log.

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() -> std::io::Result<()> {
                     let buf = &mut buf[..buf_size];
                     let req_msg = str::from_utf8(&buf).unwrap();
                     let message = format!("request message: {0: >8} {1} {2}", buf_size, src_addr, req_msg);
-                    println!("{}", &message);
+                    //println!("{}", &message);
                     writeln!(file, "{}", &message).unwrap();
                 });
             }
